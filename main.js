f681fd2d8a77823a97890869f68f532545401fcb
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2023-08-31 18:26:15 -0400

    adicionado o tailwindcss

--- main.js
+++ main.js
@@ -67,8 +67,8 @@ const catalogo = [
 
 for (const produtoCatalogo of catalogo) {
         const cartaoProduto = `
-            <div id="card_produto-1">
-            <img src="assets/img/${ produtoCatalogo.Imagem}" alt="Polo cor de Areia">
+            <div class='border-solid border-2 border-sky-500 w-48 m-2' id="card-produto-${produtoCatalogo.id}">
+            <img src="assets/img/${ produtoCatalogo.Imagem}" alt="Polo cor de Areia" style="height: 300px">
             <p>${ produtoCatalogo.marca}</p>
             <p>${ produtoCatalogo.nome}</p>
             <p>$${ produtoCatalogo.preco}</p>
