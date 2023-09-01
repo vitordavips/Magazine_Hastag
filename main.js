16af2875862d99e29cd374589d401c3627feeb7e
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2023-08-31 20:51:05 -0400

    terminando de fazer a funcionalidade do carrinho

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { inicializarCarrinho } from "./src/menuCarrinho";
+
 const catalogo = [
     {
         id: 1,
@@ -77,5 +79,6 @@ for (const produtoCatalogo of catalogo) {
         `;
 
     document.getElementById("container-produto").innerHTML += cartaoProduto
-}
+};
 
+inicializarCarrinho();
