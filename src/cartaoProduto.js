56810f0eb545e787aa60870c5dc1c73f0721a8ab
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2023-09-05 20:05:14 -0400

    ajeitando o buttom dos produtos

--- src/cartaoProduto.js
+++ src/cartaoProduto.js
@@ -1,12 +1,16 @@
+import { catalogo } from "./utilidades";
+
 export function renderizarCatalago() {
     for (const produtoCatalogo of catalogo) {
         const cartaoProduto = `
-            <div class='border-solid border-2 border-sky-500 w-48 m-2' id="card-produto-${produtoCatalogo.id}">
-            <img src="assets/img/${ produtoCatalogo.Imagem}" alt="Polo cor de Areia" style="height: 300px">
-            <p>${ produtoCatalogo.marca}</p>
-            <p>${ produtoCatalogo.nome}</p>
-            <p>$${ produtoCatalogo.preco}</p>
-            <button>Adicionar</button>
+            <div class='border-solid border-2 border-sky-500 w-48 m-2 flex flex-col p-2 justify-between group' id="card-produto-${produtoCatalogo.id}">
+                <img src="assets/img/${ produtoCatalogo.Imagem}" alt="Polo cor de Areia">
+                <p>${ produtoCatalogo.marca}</p>
+                <p>${ produtoCatalogo.nome}</p>
+                <p>$${ produtoCatalogo.preco}</p>
+                <button class='bg-slate-950 hover:bg-slate-700 hover:scale-110 text-slate-200'>
+                    <i class="fa-solid fa-cart-plus"></i>
+                </button>
             </div>
         `;
 
