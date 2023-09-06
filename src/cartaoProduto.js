33ff53bda1e53890a299763140da6e765cbc3a2b
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2023-09-05 20:20:40 -0400

    modificando o layout do cartão

--- src/cartaoProduto.js
+++ src/cartaoProduto.js
@@ -3,12 +3,16 @@ import { catalogo } from "./utilidades";
 export function renderizarCatalago() {
     for (const produtoCatalogo of catalogo) {
         const cartaoProduto = `
-            <div class='border-solid border-2 border-sky-500 w-48 m-2 flex flex-col p-2 justify-between group' id="card-produto-${produtoCatalogo.id}">
-                <img src="assets/img/${ produtoCatalogo.Imagem}" alt="Polo cor de Areia">
-                <p>${ produtoCatalogo.marca}</p>
-                <p>${ produtoCatalogo.nome}</p>
-                <p>$${ produtoCatalogo.preco}</p>
-                <button class='bg-slate-950 hover:bg-slate-700 hover:scale-110 text-slate-200'>
+            <div class='border-solid w-48 m-2 flex flex-col p-2 justify-between shadow-xl shadow-slate-400 rounded-lg group' id="card-produto-${produtoCatalogo.id}">
+                <img 
+                src="assets/img/${ produtoCatalogo.Imagem}" 
+                alt="Polo cor de Areia"
+                class='group-hover:scale-110 duration-300 my-3'
+                />
+                <p class='text-sm'>${ produtoCatalogo.marca}</p>
+                <p class='text-sm'>${ produtoCatalogo.nome}</p>
+                <p class='text-sm'>$${ produtoCatalogo.preco}</p>
+                <button class='bg-slate-950 hover:bg-slate-700 text-slate-200'>
                     <i class="fa-solid fa-cart-plus"></i>
                 </button>
             </div>
