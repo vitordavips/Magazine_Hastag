8c8508088cad0acd0a1e8f77f4a677d5ab66fed4
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2023-09-13 16:36:27 -0400

    fazendo modificações no cartaoProduto

--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -21,14 +21,19 @@ export function inicializarCarrinho() {
 
 export function adicionarAoCarrinho(idProduto){
     //Ache um produto(p) tal que esse produto(p) tenha o id igual ao idProduto
-    const produto = catalogo.find((p) => p.id === idProduto);
+    const produto = catalogo.find((p) => p.id === idProduto)
     const containerProdutoCarrinho = document.getElementById("produtos-carrinho");
     const cartaoProdutoCarrinho = `<article class="flex bg-slate-100 rounded-lg p-1 relative">
     <button id="fechar-carrinho" class="absolute top-0 right-2">
       <i class="fa-regular fa-circle-xmark text-slate-500 hover:text-slate-800"></i>
     </button>
-    <img src="assets/img/${produto.Imagem}" alt="Carrinho: ${produto.nome}" class="h-24 rounded-lg">
+    <img 
+      src"./assets/img/${produto.Imagem}" 
+      alt="Carrinho: ${produto.nome}" 
+      class="h-24 rounded-lg"
+    />
     <div class="py-2">
+      <p class="text-slate-900 text-sm">${produto.nome}</p> 
       <p class="text-slate-900 text-sm">${produto.nome}</p>
       <p class="text-slate-400 text-xs">Tamanho: M</p>
       <p class="text-green-700 text-lg">$${produto.preco}</p>
